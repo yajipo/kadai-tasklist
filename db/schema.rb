dfe6bd7fd847661ecbe71b0ae5008880811ee32e
Yaji.N <combo_modem@yahoo.co.jp>
2019-03-13 16:25:06 +0000

    finish

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20190312163228) do
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "title"
+    t.string   "status"
   end
 
 end

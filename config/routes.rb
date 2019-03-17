fb7cc229f04dacd6ba18a2be72dcb053655483d2
Yaji.N <combo_modem@yahoo.co.jp>
2019-03-17 08:45:06 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
-# Rails.application.routes.draw do
-#   root to: 'tasks#index'
-
-#   resources :tasks
-# end
 Rails.application.routes.draw do
   root to: 'toppages#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
